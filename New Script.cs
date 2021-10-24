6c1d132d1234bc70eb10507c224ad3416fc6c944
Yung_Hafner <89962794+YungHafner@users.noreply.github.com>
2021-10-24 13:59:12 +1000

    Add files via upload

---
+++ New Script.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+class Script
+{
+    [STAThread]
+    static public void Main(string[] args)
+    {
+        {
+			{
+		      bool fw = false;
+			  Console.WriteLine("vvedi text");
+			  string words= Console.ReadLine();
+			  char [] Splitchars = new char [] {' ',' ','.'};
+			  string[] word =words.Split(Splitchars,StringSplitOptions.RemoveEmptyEntries);
+			  for(int i= 0; i<word.Length; i++)
+			  {
+				  if(word[i].Length==3)
+					  Console.WriteLine(word[i]);
+				  
+			  }
+
+			  Console.ReadLine();
+			}		
+		}
+	}
+}
